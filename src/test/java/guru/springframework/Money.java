585581d6983b4ff93233466f8570d27f7d11a92c
malyskok <kamil.malysko@gmail.com>
2023-02-04 09:45:41 +0100

    improving equals that dollar and franc are not equal with the same amount

--- src/test/java/guru/springframework/Money.java
+++ src/test/java/guru/springframework/Money.java
@@ -6,6 +6,7 @@ public class Money {
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
-        return this.amount == money.amount;
+        return this.amount == money.amount
+                && this.getClass().equals(money.getClass());
     }
 }

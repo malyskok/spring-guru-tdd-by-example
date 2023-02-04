4828474b1345fd92e25f60f566b4eb34a6e2b9b6
malyskok <kamil.malysko@gmail.com>
2023-02-04 09:28:07 +0100

    adding Franc

---
+++ src/test/java/guru/springframework/Franc.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Franc franc = (Franc) o;
+        return this.amount == franc.amount;
+    }
+}
